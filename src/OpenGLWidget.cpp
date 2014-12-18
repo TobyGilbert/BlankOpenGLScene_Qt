936bf1e59717cbbcc01ac6c04fd9f56d17b68a86
Toby Gilbert <tobygilbert@hotmail.co.uk>
2014-12-18 22:38:57 +0000

    initial commit

---
+++ src/OpenGLWidget.cpp
@@ -0,0 +1,168 @@
+#include <QGuiApplication>
+
+#include "OpenGLWidget.h"
+#include <iostream>
+
+const static float INCREMENT=0.01;
+
+OpenGLWidget::OpenGLWidget(const QGLFormat _format, QWidget *_parent) : QGLWidget(_format,_parent){
+    // set this widget to have the initial keyboard focus
+    setFocus();
+    // re-size the widget to that of the parent (in this case the GLFrame passed in on construction)
+    m_rotate=false;
+    // mouse rotation values set to 0
+    m_spinXFace=0;
+    m_spinYFace=0;
+    // re-size the widget to that of the parent (in this case the GLFrame passed in on construction)
+    this->resize(_parent->size());
+}
+//----------------------------------------------------------------------------------------------------------------------
+OpenGLWidget::~OpenGLWidget(){
+    delete m_cam;
+    delete m_shaderProgram;
+    delete m_model;
+}
+//----------------------------------------------------------------------------------------------------------------------
+void OpenGLWidget::initializeGL(){
+
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+    // enable depth testing for drawing
+    glEnable(GL_DEPTH_TEST);
+    // enable multisampling for smoother drawing
+    //glEnable(GL_MULTISAMPLE);
+
+    // as re-size is not explicitly called we need to do this.
+    glViewport(0,0,width(),height());
+
+    // Initialise the model matrix
+    m_modelMatrix = glm::mat4(1.0);
+
+    // Create a shader program
+    m_shaderProgram = new ShaderProgram();
+    m_vertexShader = new Shader("shaders/PhongVert.glsl", GL_VERTEX_SHADER);
+    m_fragmentShader = new Shader("shaders/PhongFrag.glsl", GL_FRAGMENT_SHADER);
+
+    m_shaderProgram->attachShader(m_vertexShader);
+    m_shaderProgram->attachShader(m_fragmentShader);
+    m_shaderProgram->bindFragDataLocation(0, "fragColour");
+    m_shaderProgram->link();
+    m_shaderProgram->use();
+
+    delete m_vertexShader;
+    delete m_fragmentShader;
+
+    m_model = new Model("models/sphere.obj");
+    //m_model = new Model();
+    //m_model->loadCube();
+
+    m_projLoc = m_shaderProgram->getUniformLoc("projectionMatrix");
+    m_normalLoc = m_shaderProgram->getUniformLoc("normalMatrix");
+    m_modelViewProjectionLoc = m_shaderProgram->getUniformLoc("modelViewProjectionMatrix");
+
+    GLuint lightPosLoc = m_shaderProgram->getUniformLoc("light.position");
+    GLuint lightIntLoc = m_shaderProgram->getUniformLoc("light.intensity");
+    GLuint kdLoc = m_shaderProgram->getUniformLoc("Kd");
+    GLuint kaLoc = m_shaderProgram->getUniformLoc("Ka");
+    GLuint ksLoc = m_shaderProgram->getUniformLoc("Ks");
+    GLuint shininessLoc = m_shaderProgram->getUniformLoc("shininess");
+
+    glUniform4f(lightPosLoc, 1.0, 1.0, 1.0, 1.0);
+    glUniform3f(lightIntLoc, 0.8, 0.8, 0.8);
+    glUniform3f(kdLoc, 0.5, 0.5, 0.5);
+    glUniform3f(kaLoc, 0.5, 0.5, 0.5);
+    glUniform3f(ksLoc, 1.0, 1.0, 1.0);
+    glUniform1f(shininessLoc, 100.0);
+
+    // Initialize the camera
+    m_cam = new Camera(glm::vec3(0.0, 0.0, 5.0));
+
+    startTimer(0);
+
+}
+//----------------------------------------------------------------------------------------------------------------------
+void OpenGLWidget::resizeGL(const int _w, const int _h){
+    // set the viewport for openGL
+    glViewport(0,0,_w,_h);
+    m_cam->setShape(_w, _h);
+
+}
+//----------------------------------------------------------------------------------------------------------------------
+void OpenGLWidget::timerEvent(QTimerEvent *){
+    updateGL();
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+void OpenGLWidget::paintGL(){
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //Initialise the model matrix
+
+    glm::mat4 rotx;
+    glm::mat4 roty;
+
+    rotx = glm::rotate(rotx, m_spinXFace, glm::vec3(1.0, 0.0, 0.0));
+    roty = glm::rotate(roty, m_spinYFace, glm::vec3(0.0, 1.0, 0.0));
+
+    m_mouseGlobalTX = rotx*roty;
+    m_mouseGlobalTX = glm::translate(m_mouseGlobalTX, glm::vec3(m_modelPos.x, m_modelPos.y, m_modelPos.z));
+    //m_modelMatrix = m_mouseGlobalTX;
+
+    //m_modelMatrix = glm::scale(m_modelMatrix, glm::vec3(10.0, 10.0, 10.0));
+    loadMatricesToShader();
+
+    glBindVertexArray(m_model->getVAO());
+    glDrawArrays(GL_TRIANGLES, 0, m_model->getNumVerts());
+    glBindVertexArray(0);
+
+}
+
+void OpenGLWidget::mouseMoveEvent (QMouseEvent * _event)
+{
+  // note the method buttons() is the button state when event was called
+  // this is different from button() which is used to check which button was
+  // pressed when the mousePress/Release event is generated
+  if(m_rotate && _event->buttons() == Qt::LeftButton)
+  {
+    int diffx=_event->x()-m_origX;
+    int diffy=_event->y()-m_origY;
+    m_spinXFace += (float) 0.5f * diffy;
+    m_spinYFace += (float) 0.5f * diffx;
+    m_origX = _event->x();
+    m_origY = _event->y();
+
+  }
+        // right mouse translate code
+  else if(m_translate && _event->buttons() == Qt::RightButton)
+  {
+    int diffX = (int)(_event->x() - m_origXPos);
+    int diffY = (int)(_event->y() - m_origYPos);
+    m_origXPos=_event->x();
+    m_origYPos=_event->y();
+    m_modelPos.x += INCREMENT * diffX;
+    m_modelPos.y -= INCREMENT * diffY;
+
+   }
+}
+//----------------------------------------------------------------------------------------------------------------------
+void OpenGLWidget::loadMatricesToShader(){
+
+    // Calculate projection matrix
+
+    //m_modelMatrix = glm::scale(m_modelMatrix, glm::vec3(10.0, 10.0, 10.0));
+    glm::mat4 projectionMatrix = m_cam->getProjectionMatrix();
+
+    glm::mat4 modelViewMatrix = m_cam->getViewMatrix() * m_modelMatrix;
+
+    m_normalMatrix = glm::mat3(modelViewMatrix);
+    m_normalMatrix = glm::inverse(m_normalMatrix);
+    m_normalMatrix = glm::transpose(m_normalMatrix);
+
+    m_modelViewProjectionMatrix = projectionMatrix * modelViewMatrix;
+
+    //glUniformMatrix4fv(m_modelLoc, 1, false, glm::value_ptr(m_modelMatrix));
+    glUniformMatrix4fv(m_modelViewLoc, 1, false, glm::value_ptr(modelViewMatrix));
+    glUniformMatrix4fv(m_projLoc, 1, false, glm::value_ptr(projectionMatrix));
+    glUniformMatrix3fv(m_normalLoc, 1, false, glm::value_ptr(m_normalMatrix));
+    glUniformMatrix4fv(m_modelViewProjectionLoc, 1, false, glm::value_ptr(m_modelViewProjectionMatrix));
+}
+
